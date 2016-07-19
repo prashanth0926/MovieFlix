f5a3bdb21b31431e31f57ee4a9cbef959da7f7b3
mpnaidu26 <pmolakal@hawk.iit.edu>
2016-07-19 12:21:19 -0500

    feat(module-api): connection to mongodb and routing in app.js

--- server/MovieFlix_REST_API/app.js
+++ server/MovieFlix_REST_API/app.js
@@ -5,7 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+var passport = require('passport');
+var localStrategy = require('passport-local');
+var config = require('./config');
+
+mongoose.connect(config.mongoUrl);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Connected correctly to server");
+});
+
 var routes = require('./routes/index');
+var movies = require('./routes/movies');
 var users = require('./routes/users');
 
 var app = express();
@@ -20,10 +33,19 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// passport config
+var user = require('./models/users');
+app.use(passport.initialize());
+passport.use(new localStrategy(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/movies', movies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/MovieFlix_REST_API/config.js
+++ server/MovieFlix_REST_API/config.js
@@ -3,5 +3,6 @@
  */
 
 module.exports = {
-    'secretKey': '12345-67890-09876-54321'
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/MovieFlix'
 }
